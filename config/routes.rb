70695e9a0f662805f99926344acde12e812dbbc5
Melky Dominguez <melky92@hotmail.com>
2018-11-11 20:42:44 -0400

    Adicionar encabezado y página principal

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :clientes
   resources :usuarios
   resources :tipo_usuarios
+  root 'usuarios#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
