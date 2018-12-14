96b984fe87bfd05c07493219b2975a5099738c17
Melky Dominguez <melky92@hotmail.com>
2018-12-13 21:08:30 -0400

    Adicionar marcadores de mapa

--- app/controllers/imagenes_controller.rb
+++ app/controllers/imagenes_controller.rb
@@ -1,9 +1,16 @@
 class ImagenesController < ApplicationController
-    def ver
-      path = "/srv/obras-scz/imagenes/#{params[:filename]}.jpg"
-      send_file( path,
-        :disposition => 'inline',
-        :type => 'image/jpeg',
-        :x_sendfile => true )
-    end
+  def ver
+    path = "/srv/obras-scz/imagenes/#{params[:filename]}.jpg"
+    send_file( path,
+      :disposition => 'inline',
+      :type => 'image/jpeg',
+      :x_sendfile => true )
   end
+  def icono
+    path = "/srv/obras-scz/iconos/#{params[:id]}.png"
+    send_file( path,
+      :disposition => 'inline',
+      :type => 'image/png',
+      :x_sendfile => true )
+  end
+end
