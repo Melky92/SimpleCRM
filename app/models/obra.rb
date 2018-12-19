6e7c1cf798a0f875a114d938c4f64cef97867d17
Melky Dominguez <melky92@hotmail.com>
2018-12-19 16:10:49 -0400

    Aumentar posibilidad de ver quien reporto la obra en detalles

--- app/models/obra.rb
+++ app/models/obra.rb
@@ -1,4 +1,5 @@
 class Obra < ApplicationRecord
   belongs_to :cliente, optional: true
   belongs_to :usuario, optional: true
+  belongs_to :reportado_por, :class_name => "Usuario" ,:foreign_key => "reportado_por", optional: true
 end
